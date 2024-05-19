5862ac9a425f77a706736a45e5a65dc1f631c6f1
JORD-Reda <84501157+JORD-Reda@users.noreply.github.com>
2024-05-19 14:47:29 +0100

    login & register

--- backend/demo/src/main/java/com/example/demo/ws/UserController.java
+++ backend/demo/src/main/java/com/example/demo/ws/UserController.java
@@ -1,17 +1,15 @@
 package com.example.demo.ws;
 
-
 import com.example.demo.bean.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/users")// Update this to match your front-end origin
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
@@ -21,10 +19,12 @@ public class UserController {
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
+
     @PostMapping("/login")
-    public boolean authenticateUser(@RequestParam String email, @RequestParam String password) {
-        return userService.authenticate(email, password);
+    public boolean authenticateUser(@RequestBody LoginRequest loginRequest) {
+        return userService.authenticate(loginRequest.getEmail(), loginRequest.getPassword());
     }
+
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
@@ -40,5 +40,3 @@ public class UserController {
         userService.deleteUser(id);
     }
 }
-
-
